a2dc06eaaacd76483e00eabddc7594610bc46f96
Jeremy Qin <jeremyqin1@gmail.com>
2022-08-23 00:00:49 -0400

    global pooling for cifar

--- cnn.py
+++ cnn.py
@@ -6,23 +6,23 @@ class BasicNet(nn.Module):
     def __init__(self, num_classes, in_channels):
         super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
             nn.ReLU(),
             nn.AdaptiveMaxPool2d(1))
         
-        self.fc1 = nn.Linear(128 * 4, 64)
+        self.fc1 = nn.Linear(256, 128)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(64, 32)
+        self.fc2 = nn.Linear(128, 64)
         self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(32, num_classes)
+        self.fc3 = nn.Linear(64, num_classes)
         
     def forward(self, x):
         out = self.layer1(x)
